c2aa334f071e7f6aa49cf701cabfbea4c1e4dded
Konstantin Ostrovskiy <k.ostrovskiy2@atol.ru>
2023-01-28 20:54:43 +0800

    Task: createGetter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,16 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  return function(obj) {
+    const checkNextLevel = (path, data) => {
+      if (path.length <= 1) {
+        return data?.[path[0]] || undefined;
+      }
 
+      const [nextPath, ...otherPath] = path;
+      return checkNextLevel(otherPath, data[nextPath]);
+    };
+
+    return checkNextLevel(path.split('.'), obj);
+  };
 }
