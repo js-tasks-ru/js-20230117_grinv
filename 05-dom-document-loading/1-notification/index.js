226934015f9f978ebdffc1f37d676fdd8ab3bb4b
Konstantin Ostrovskiy <k.ostrovskiy2@atol.ru>
2023-02-10 17:12:53 +0800

    Notification

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,45 @@
 export default class NotificationMessage {
+  static notification = null;
+  
+  constructor(message = '', config = {}) {
+    this.message = message;
+    this.duration = config.duration;
+    this.type = config.type;
+    this.createNotification();
+  }
+  
+  createNotification() {
+    const wrapper = document.createElement('div');
+    wrapper.innerHTML = `
+      <div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.type}</div>
+          <div class="notification-body">
+            ${this.message}
+          </div>
+        </div>
+      </div>
+    `;
+    this.element = wrapper.firstElementChild;
+  }
+  
+  show(target = document.body) {
+    if (NotificationMessage.notification) {
+      clearTimeout(NotificationMessage.notification.timeout);
+      NotificationMessage.notification.remove();
+    }
+    
+    NotificationMessage.notification = this;
+    this.timeout = setTimeout(this.remove.bind(this), this.duration);
+    target.append(this.element);
 
+  }
+  remove() {
+    this.element.remove();
+  }
+  
+  destroy() {
+    this.remove();
+  }
 }
